af5f8e727df7d3600de52de929208e55b9b6be76
BETHA\bruno.kurzawe <compras@4>
2023-10-08 12:54:56 -0300

    Adicionado persistencia de dados

--- src/main/java/com/satc/satcloja/resource/ProdutoController.java
+++ src/main/java/com/satc/satcloja/resource/ProdutoController.java
@@ -4,12 +4,10 @@ import com.satc.satcloja.model.Produto;
 import com.satc.satcloja.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 
 @RestController
@@ -23,6 +21,18 @@ public class ProdutoController {
     public ResponseEntity create(@RequestBody Produto entity) {
         Produto save = service.salvar(entity);
         return ResponseEntity.created(URI.create("/api/produtos/" + entity.getId())).body(save);
+    }
+
+    @GetMapping
+    public ResponseEntity findAll() {
+        List<Produto> produtos = service.buscaTodos();
+        return ResponseEntity.ok(produtos);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity findById(@PathVariable("id") Long id) {
+        Produto produto = service.buscaPorId(id);
+        return ResponseEntity.ok(produto);
     }
 }
 
